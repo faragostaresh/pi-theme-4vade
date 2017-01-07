80fdd6fdeb7b8988affef10632188c923cd43812
voltan <djvoltan@gmail.com>
2017-01-07 16:15:55 +0330

    Import theme

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+/**
+ * Meta for Pi Theme
+ *
+ * A strict HTML 5 theme inherited from Default Theme
+ */
+return array(
+    // Version
+    'version'       => '1.0.1',
+    // Type of layouts available in the theme
+    'type'          => 'front',
+    // Title of the theme
+    'title'         => '4vade',
+    // Author information: name, email, website
+    'author'        => 'Architecture: Hossein Azizabadi <azizabadi@faragostaresh.com>;',
+    // Screenshot image
+    'screenshot'    => 'image/screenshot.png',
+    // License or theme images and scripts
+    'license'       => 'Creative Common License',
+    // Optional description
+    'description'   => '4vade.com HTML5 theme.',
+    // Parent theme from which templates can be inherited, default as 'default'
+    'parent'        => 'default',
+    // Supported browsers
+    'browser'       => 'Internet Explorer: 8+; Chrome: latest;'
+                     . ' Firefox: latest; Safari: latest; Opera: latest;',
+);
